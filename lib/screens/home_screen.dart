4a891f70f6d80d7ca05a70eab1b57d6bacc0af57
micahyanoh <ogongoyanojnr@gmail.com>
2024-06-09 22:34:25 +0300

    wozza

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 20), //const EdgeInsets.only(right: 20, left: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Good Morning',
+                          style: Styles.headLinestyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          'Book Tickets',
+                          style: Styles.headLinestyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                  "assets/images/adventist-symbol--denim.png"))),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
